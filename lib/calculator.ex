d20e66e3598a4ed7b6983303df460646b92de4f5
johnpires <john.pires81@gmail.com>
2021-11-16 22:11:38 -0500

    changes in names

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -7,12 +7,12 @@ defmodule Calculator do
   Start the system to process some result.
   """
   def start() do
-    joao = %Student{name: "João Joaquim", results: build_subjects([5.2, 3.9, 2.7, 9.8])}
-    maria = %Student{name: "Maria da Silva", results: build_subjects([8.4, 7.5, 9.4, 2.8])}
-    pedro = %Student{name: "Pedro Pedrada", results: build_subjects([1.3, 8.5, 8.9, 7.6])}
-    kaua = %Student{name: "Kauã Camboinhas", results: build_subjects([5.4, 4.9, 2.2, 3.8])}
+    joao = %Student{name: "João Victor", results: build_subjects([5.2, 3.9, 2.7, 9.8])}
+    maria = %Student{name: "Maria do Socorro", results: build_subjects([8.4, 7.5, 9.4, 2.8])}
+    adam = %Student{name: "Adam Andrade", results: build_subjects([1.3, 8.5, 8.9, 7.6])}
+    cris = %Student{name: "Cristiano da Silva", results: build_subjects([5.4, 4.9, 2.2, 3.8])}
 
-    result = calculate([joao, maria, pedro, kaua])
+    result = calculate([joao, maria, adam, cris])
     best = best_result(result)
 
     {result, best}
@@ -71,7 +71,7 @@ defmodule Calculator do
   - `results` - A list of results
   """
   defp build_subjects(results) do
-    subjects = ["Matemática", "Português", "Geografia", "História"]
+    subjects = ["Math", "Portuguese", "Geography", "History"]
 
     subjects
     |> Enum.map(&%Subject{name: &1, result: Enum.random(results)})
